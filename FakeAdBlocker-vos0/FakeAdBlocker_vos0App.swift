aaa962162bfd1ca4e81f7c114f966c5cf02fb77f
Devesh Rai <devesh@gmail>
2024-05-21 22:25:40 +0530

    Initial Commit

---
+++ FakeAdBlocker-vos0/FakeAdBlocker_vos0App.swift
@@ -0,0 +1,17 @@
+//
+//  FakeAdBlocker_vos0App.swift
+//  FakeAdBlocker-vos0
+//
+//  Created by Devesh Rai on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FakeAdBlocker_vos0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
